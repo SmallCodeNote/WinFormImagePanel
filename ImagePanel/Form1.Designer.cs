edf2d676c3cb9b149cb45a51c9ad0b806f7da570
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-01-09 05:30:46 +0900

    Add files via upload

---
+++ ImagePanel/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ImagePanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_LoadImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(81, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_LoadImage
+            // 
+            this.button_LoadImage.Location = new System.Drawing.Point(500, 58);
+            this.button_LoadImage.Name = "button_LoadImage";
+            this.button_LoadImage.Size = new System.Drawing.Size(83, 35);
+            this.button_LoadImage.TabIndex = 1;
+            this.button_LoadImage.Text = "LoadImage";
+            this.button_LoadImage.UseVisualStyleBackColor = true;
+            this.button_LoadImage.Click += new System.EventHandler(this.button_LoadImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 865);
+            this.Controls.Add(this.button_LoadImage);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_LoadImage;
+    }
+}
+
---
+++ ImagePanel/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagePanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBoxScroll pictureBoxScroll;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBoxScroll = new PictureBoxScroll(panel1);    
+        }
+
+        private void button_LoadImage_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            pictureBoxScroll.setPicture(new Bitmap(ofd.FileName));
+
+        }
+    }
+}
